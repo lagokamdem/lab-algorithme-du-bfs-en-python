c7f3683bb9822e8e38a78227d62a741fd3ce9330
unknown <lagokamdem87@gmail.com>
2020-03-31 15:22:29 -0400

    La taille de la queue est <4>

--- b000000000.py
+++ b000000000.py
@@ -19,25 +19,19 @@ eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
 
-def personne_elue (name):
-    return name =='Zoureni'
-
-
-def search(name):  
-   print( len(eleves.values()))
-   return False
-
-if __name__== "__main__":
-    search("Boris")
-
+def personne_elue(name):
+    return name == 'Zoureni'
 
 from collections import deque
 
 def search(name):
-    search_queue = deque()
-    search_queue += eleves [name]
-    print( len(search_queue))
-    return False
+   search_queue = deque()
+   search_queue+=eleves [name]
+   print( len(search_queue))
+   return False
+
+if __name__== "__main__":
+  search("Boris")
 
 
 
